40c1aec0d4eb7db951857ab62535e625c4f199d2
GRAFFAN Jérémy <jeremy.graffan@etu.univ-amu.fr>
2020-10-23 22:12:31 +0200

    Init V2

--- src/fr/isola/IsolaGame.java
+++ src/fr/isola/IsolaGame.java
@@ -3,7 +3,7 @@ package fr.isola;
 class IsolaGame {
 
     public static void main(String[] args) {
-        System.out.println("HELLO WORLD");
+        System.out.println("HELLO WORLD V2");
     }
     
 }
